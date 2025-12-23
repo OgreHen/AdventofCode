8991fea6236fb9dc0c9996935c57dfd620b54d34
OgreHen <nehergo@gmail.com>
2025-12-23 16:32:05 +0100

    First try

--- 2016/day1.py
+++ 2016/day1.py
@@ -12,15 +12,41 @@ def part_1_and_2(start):
         else:
             direction = (direction - 1) % 4
         if direction == 0:
+            if location == []:
+                for i in range(n_s + 1, n_s + int(step[1:]) + 1):
+                    if [e_w, i] in coordinates:
+                        location = [e_w, i]
+                        break
+                    else:
+                        coordinates.append([e_w, i])
             n_s += int(step[1:])
         elif direction == 1:
+            if location == []:
+                for i in range(e_w + 1, e_w + int(step[1:]) + 1):
+                    if [i, n_s] in coordinates:
+                        location = [i, n_s]
+                        break
+                    else:
+                        coordinates.append([i, n_s])
             e_w += int(step[1:])
         elif direction == 2:
+            if location == []:
+                for i in range(n_s - int(step[1:]), n_s):
+                    if [e_w, i] in coordinates:
+                        location = [e_w, i]
+                        break
+                    else:
+                        coordinates.append([e_w, i])
             n_s -= int(step[1:])
         else:
+            if location == []:
+                for i in range(e_w- int(step[1:]), e_w):
+                    if [i, n_s] in coordinates:
+                        location = [i, n_s]
+                        break
+                    else:
+                        coordinates.append([i, n_s])
             e_w -= int(step[1:])
-        coordinates.append([e_w, n_s])
-    location = [0, 0]
     return abs(n_s) + abs(e_w), abs(location[0]) + abs(location[1])
 
 
